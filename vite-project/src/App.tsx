2c8b8457c4d3a0d047d19e074bd54e2f49ce351a
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-26 14:08:45 +0400

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import { Footer, Header } from "./layouts";
+
+import { Home, About, Design, Contact, Location } from "./pages";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/design/:category" element={<Design />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/location" element={<Location />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
